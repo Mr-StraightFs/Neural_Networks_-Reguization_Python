090dcb81f1f43c5de6ca4bb1e7506994e542f7de
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-06-06 18:31:07 +0100

    Cost with Reg

--- Regularization_.py
+++ Regularization_.py
@@ -13,3 +13,86 @@ from public_tests import *
 plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
+
+# Load the Dataset
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
+
+# The main function for cost calculation (retuens params dependin on the chosen cost function , and the cost plot
+def model(X, Y, learning_rate=0.3, num_iterations=30000, print_cost=True, lambd=0, keep_prob=1):
+    grads = {}
+    costs = []  # to keep track of the cost
+    m = X.shape[1]  # number of examples
+    layers_dims = [X.shape[0], 20, 3, 1]
+
+    # Initialize parameters dictionary.
+    parameters = initialize_parameters(layers_dims)
+
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        if keep_prob == 1:
+            a3, cache = forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+
+        # Cost function
+        if lambd == 0:
+            cost = compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+
+        # Backward propagation.
+        assert (lambd == 0 or keep_prob == 1)  # it is possible to use both L2 regularization and dropout,
+        # but this assignment will only explore one at a time
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(X, Y, cache)
+        elif lambd != 0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Print the loss every 10000 iterations
+        if print_cost and i % 10000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+        if print_cost and i % 1000 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+return parameters
+
+# Calculate the Cost with the regulirasation term , (Lambda)
+
+def compute_cost_with_regularization (A3, Y, parameters, lambd):
+ m = Y.shape[1]
+ W1 = parameters["W1"]
+ W2 = parameters["W2"]
+ W3 = parameters["W3"]
+return cost
+
+
+
+cross_entropy_cost = compute_cost(A3, Y)  # This gives you the cross-entropy part of the cost
+
+    L2_regularization_cost = (1 / m) * (lambd / 2) * (np.sum(np.square(W1))) + (np.sum(np.square(W2))) + np.sum(np.square(W3)))
+
+    cost = cross_entropy_cost + L2_regularization_cost
+
+# Testing the regulirized Model
+A3, t_Y, parameters = compute_cost_with_regularization_test_case()
+cost = compute_cost_with_regularization(A3, t_Y, parameters, lambd=0.1)
+print("cost = " + str(cost))
+
+compute_cost_with_regularization_test(compute_cost_with_regularization)
+
